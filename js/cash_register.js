458383242f389cd935f0c0c41fefe395475ab824
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2017-12-30 13:27:39 -1000

    add variables to get elements by ID

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,1 +1,33 @@
 console.log("register sanity check");
+
+(function cashRegister() {
+
+    let zeroBtn = document.getElementById("zero");
+    let doubleZeroBtn = document.getElementById("doubleZero");
+    let decimalBtn = document.getElementById("cents");
+    let oneBtn = document.getElementById("one");
+    let twoBtn = document.getElementById("two");
+    let threeBtn = document.getElementById("three");
+    let fourBtn = document.getElementById("four");
+    let fiveBtn = document.getElementById("five");
+    let sixBtn = document.getElementById("six");
+    let sevenBtn = document.getElementById("seven");
+    let eightBtn = document.getElementById("eight");
+    let nineBtn = document.getElementById("nine");
+    let divideBtn = document.getElementById("divide");
+    let multiplyBtn = document.getElementById("multiply");
+    let addBtn = document.getElementById("add");
+    let subtractBtn = document.getElementById("subtract");
+    let totalBtn = document.getElementById("total");
+    let clearBtn = document.getElementById("clear");
+    let subtotalBtn = document.getElementById("getBalance");
+    let depositBtn = document.getElementById("deposit");
+    let withdrawBtn = document.getElementById("withdraw");
+     
+    
+
+
+
+
+
+})();
