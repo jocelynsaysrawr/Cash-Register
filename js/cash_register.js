128ba67dc20751f6f1a491a014bfa7a4c159db3f
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-01-02 19:36:12 -1000

    add operation button functions

--- js/cash_register.js
+++ js/cash_register.js
@@ -2,58 +2,97 @@ console.log("register sanity check");
 
 function cashRegister() {
 
-    let dollars = 0;
-    let cents = 0;
-
-    let zeroBtn = document.getElementById("zero");
-    let doubleZeroBtn = document.getElementById("doubleZero");
-    let decimalBtn = document.getElementById("cents");
-    let oneBtn = document.getElementById("one");
-        oneBtn.value = Number(oneBtn.innerHTML);
-        console.log(oneBtn.value);
-
-    let twoBtn = document.getElementById("two");
-    let threeBtn = document.getElementById("three");
-    let fourBtn = document.getElementById("four");
-    let fiveBtn = document.getElementById("five");
-    let sixBtn = document.getElementById("six");
-    let sevenBtn = document.getElementById("seven");
-    let eightBtn = document.getElementById("eight");
-    let nineBtn = document.getElementById("nine");
-    let divideBtn = document.getElementById("divide");
-    let multiplyBtn = document.getElementById("multiply");
-    let addBtn = document.getElementById("add");
-    let subtractBtn = document.getElementById("subtract");
     let totalBtn = document.getElementById("total");
     let clearBtn = document.getElementById("clear");
     let subtotalBtn = document.getElementById("getBalance");
     let depositBtn = document.getElementById("deposit");
     let withdrawBtn = document.getElementById("withdraw");
     
-    let getDisplay = document.getElementById("display");
-    let getAmount = document.getElementById("amount");
+    let display = document.getElementById("amount");
 
-    let getBtn = document.getElementsByTagName("button");
-     
+
+   /* function showNum() {
+        if (dollars === 0){
+            dollars += Number(this.innerHTML) / 100;
+            display.innerHTML = dollars;
+        }else if (dollars !== 0){
+           dollars = (dollars * 10) + (Number(this.innerHTML) / 100);
+           dollars = Number(dollars.toFixed(2));
+            display.innerHTML = calculator.load(dollars);
+        }   
+    }
+    */
     let numBtns = document.getElementsByClassName("num");
 
     for (var i = 0; i < numBtns.length; i++){
         numBtns[i].addEventListener("click", showNum);
-
     }
+
+    let numArray = [];
 
     function showNum() {
-        if (dollars === 0){
-            dollars += Number(this.innerHTML) / 100;
-            getAmount.innerHTML = dollars;
-        }else if (dollars !== 0){
-           dollars = (dollars * 10) + (Number(this.innerHTML) / 100);
-            getAmount.innerHTML = dollars.toFixed(2);
-        }   
+        if (display.innerHTML === "0.00"){
+            display.innerHTML = this.innerHTML;
+        }else{
+            display.innerHTML += this.innerHTML;
+        }
     }
 
+    let decimalBtn = document.getElementById("cents");
+    decimalBtn.addEventListener("click", decimal)
 
+    function decimal() {
+        if (display.innerHTML.includes(".")){
+            throw Error ("too many decimals!")
+        }else{
+            display.innerHTML += this.innerHTML;
+        }
+    }
 
+    let dollars = 0;
+    let operation = null;
+
+    let addBtn = document.getElementById("add");
+    addBtn.addEventListener("click", addition);
+
+    function addition() {
+        dollars = Number(parseFloat(display.innerHTML).toFixed(2));
+        operation = "add";
+        console.log(typeof dollars);
+        console.log(dollars);
+        calculator.add(dollars);
+        display.innerHTML = "";
+    }
+
+    let subtractBtn = document.getElementById("subtract");
+    subtractBtn.addEventListener("click", subtraction);
+
+    function subtraction() {
+        dollars = Number(parseFloat(display.innerHTML).toFixed(2));
+        operation = "subtract";
+        calculator.subtract(dollars);
+        dispaly.innerHTML = "";
+    }
+
+    let multiplyBtn = document.getElementById("multiply");
+    multiplyBtn.addEventListener("click", multiplication);
+
+    function multiplication() {
+        dollars = Number(parseFloat(display.innerHTML).toFixed(2));
+        operation = "multiply";
+        calculator.multiply(dollars);
+        display.innerHTML = "";
+    }
+
+    let divideBtn = document.getElementById("divide");
+    divideBtn.addEventListener("click", division);
+
+    function division() {
+        dollars = Number(parseFloat(display.innerHTML).toFixed(2));
+        operation = "divide";
+        calculator.divide(dollars);
+        display.innerHTML = "";
+    }
 
 }
 
